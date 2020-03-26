db989a65fbf20a5cfd8845491815e34db20c2657
syed shouzeb hassan <38203938+shouzeb@users.noreply.github.com>
2020-03-26 23:12:22 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  7 22:53:16 2020
+
+@author: Shouzeb
+"""
+
+
+__Project_Description__ = '''
+This is a python based 3x3 puzzle solver which solves the problem by using list
+which represented as 1 dimensionally. (Using 1D list is the greatest challenge for me.)
+Project has several methods to solve subproblems. The subproblems are:
+    * Calculating inversion,
+    * Checking solubility of the board,
+    * Calculating Hamming Distance,
+    * Calculating Manhattan Distance (BONUS),
+    * If board arrangement has solution than find the 'Legal Board Arrangements' -> (changing blank tile's position)
+    * Generate a heuristic function to get closest path to go to goal state.
+    * Breadth First Search and Generating the Result.'''
+
+from time import time
+from BFS_search import breadth_first_search
+from Astar_search import Astar_search
+from RBFS_search import recursive_best_first_search
+from puzzle import Puzzle
+
+
+state=[[1 , 2, 0, 3, 4, 5, 6, 7, 8]]
+
+for i in range(0,1):
+    Puzzle.num_of_instances=0
+    t0=time()
+    bfs=breadth_first_search(state[i])
+    t1=time()-t0
+    print('BFS:', bfs)
+    print('space:',Puzzle.num_of_instances)
+    print('time:',t1)
+    print()
+"""
+    Puzzle.num_of_instances = 0
+    t0 = time()
+    astar = Astar_search(state[i])
+    t1 = time() - t0
+    print('A*:',astar)
+    print('space:', Puzzle.num_of_instances)
+    print('time:', t1)
+    print()
+
+    Puzzle.num_of_instances = 0
+    t0 = time()
+    RBFS = recursive_best_first_search(state[i])
+    t1 = time() - t0
+    print('RBFS:',RBFS)
+    print('space:', Puzzle.num_of_instances)
+    print('time:', t1)
+    print()
+    
+    print('------------------------------------------')"""
