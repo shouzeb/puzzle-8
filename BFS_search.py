4265babfb53f568377cdb15c30e0d34af97d3332
syed shouzeb hassan <38203938+shouzeb@users.noreply.github.com>
2020-04-30 21:59:16 +0500

    Add files via upload

--- BFS_search.py
+++ BFS_search.py
@@ -9,22 +9,22 @@ from puzzle import Runner
 from queue import Queue
 
 def Breath_First_Search(initial_state):
-    start_node = Runner(initial_state, None, None,0,False,False,False)
+    start_node = Runner(initial_state, None, None,0)
     if start_node.goal_state_check():
         return start_node.find_solution_path()
     q=Queue()
     q.put(start_node)
-    fringe=[]
+    explored=[]
     while not(q.empty()):
         node=q.get()
-        fringe.append(q)
+        explored.append(q)
         children=node.generate_child()
         #print(children)
         for child in children:
             #print(str(child))
-            if child.state not in fringe:
-                
+            if child.state not in explored:
                 if child.goal_state_check():
+                    print("i am here")
                     return child.find_solution_path()
                 q.put(child)
     return
--- DFS_search.py
+++ DFS_search.py
@@ -7,19 +7,19 @@ Created on Fri Apr 24 23:27:36 2020
 from puzzle import Runner
 
 def depth_first_search(starting_state):
-    start_node=Runner(starting_state,None,None,0,False,False,False)
+    start_node=Runner(starting_state,None,None,0)
     if start_node.goal_state_check():
         return start_node.find_solution_path()
     s=[]
     s.append(start_node)
-    fringe=[]
+    explored=[]
     #fringe.put(start_node)
     while (len(s)!=0):
         node=s.pop()
-        fringe.append(node.state)
+        explored.append(node.state)
         children=node.generate_child()
         for child in children:
-            if child.state not in fringe and child.goal_state_check():
+            if child.state not in explored and child.goal_state_check():
                 return child.find_solution_path()
             s.append(child)
     return
