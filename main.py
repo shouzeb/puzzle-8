10becfab6ec54b6d018db55b7f98418cc0a7c045
syed shouzeb hassan <38203938+shouzeb@users.noreply.github.com>
2020-05-06 21:32:21 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from DFS_search import depth_first_search
 from Astar_search import Astar_Search
 from greedy_search import Greedy_Search
 from uniform_cost_search import uniform_cost_search
-state=[7,5,2,4,0,8,6,1,3]
+state=[5,1,2,3,0,4,6,7,8]
 
 
 print("\t\t\t>>>>>>Implementating Astar Search. Kindly wait...<<<<<<")
@@ -43,21 +43,23 @@ print('space:',Runner.number_of_steps)
 print('time:',t1)
 print()
 
-print("\t\t\t>>>>>>Implementating Breath First Search. Kindly wait...<<<<<<")
-Runner.number_of_steps=0
-t0=time()
-bfs=Breath_First_Search(state)
-t1=time()-t0
-print('BFS:', bfs)
-print('space:',Runner.number_of_steps)
-print('time:',t1)
-print()
+
 print("\t\t\t>>>>>>Implementating Deapth First Search. Kindly wait...<<<<<<")
 Runner.number_of_steps=0
 t0=time()
 dfs=depth_first_search(state)
 t1=time()-t0
 print('DFS:', dfs)
+print('space:',Runner.number_of_steps)
+print('time:',t1)
+print()
+
+print("\t\t\t>>>>>>Implementating Breath First Search. Kindly wait...<<<<<<")
+Runner.number_of_steps=0
+t0=time()
+bfs=Breath_First_Search(state)
+t1=time()-t0
+print('BFS:', bfs)
 print('space:',Runner.number_of_steps)
 print('time:',t1)
 print()
